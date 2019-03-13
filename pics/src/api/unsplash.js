e7bc7657d27b6ace26c3a40c0b7b95b0bc8bb207
Ricky MM <rickymafra@gmail.com>
2019-03-13 13:01:12 -0300

    estudando redux

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
     Authorization:
-      "Client-ID PUTIDHERE"
+      "Client-ID e2dc1ad0ffa3c6750b09e3ffb1769e5ea03515d7505d5b0d2a11d88bacb84275"
   }
 });
