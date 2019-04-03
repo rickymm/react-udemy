e881ff0d443440b4cf6adbaa6934ef37a0cc8c74
Ricky MM <rickymafra@gmail.com>
2019-04-03 16:39:29 -0300

    Add hooks-example folder, studying hooks

---
+++ hooks-simple/src/components/UserList.js
@@ -0,0 +1,18 @@
+//EXAMPLE OF REUSING CODE
+
+import React from "react";
+import useResources from "./useResources";
+
+const UserList = () => {
+  const users = useResources("users");
+
+  return (
+    <ul>
+      {users.map(user => (
+        <li key={user.id}> {user.name} </li>
+      ))}
+    </ul>
+  );
+};
+
+export default UserList;
